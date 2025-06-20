2cfaf92f13f9e65b34c8ba749edabd3d66d396f3
carious55vonmarus <diasov76487@gmail.com>
2025-06-20 16:15:31 -0400

    Add files via upload

---
+++ RobloxSpeedrun4ScriptHack.go
@@ -0,0 +1,6 @@
+// Az5ZzR8w2n
+package main
+import "fmt"
+func main() {
+    fmt.Println("Hello from function c224606afef5403ea2b95d047ab12a4c_20250619080305613008!")
+}
---
+++ RobloxSpeedrun4ScriptUtility.swift
@@ -0,0 +1,3 @@
+// Az5ZzR8w2n
+import Foundation
+print("Hello from function c224606afef5403ea2b95d047ab12a4c_20250619080305613008!")
